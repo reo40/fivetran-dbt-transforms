7135936204fd897057d7078bb8efdc35f4c21f21
reo40 <r_miyahara@jwts.co.jp>
2026-02-03 16:58:28 +0900

    Update test2.sql

--- models/test2.sql
+++ models/test2.sql
@@ -1,29 +1,52 @@
 {{ config(materialized='view') }}
 
 select
-  try_to_number(format_version) as format_version,
-  try_to_number(id) as id,
+  /* format_version NUMERIC */
+  TRY_TO_NUMBER(TO_VARCHAR(format_version)) as format_version,
+
+  /* id NUMERIC(18,0) */
+  TRY_TO_DECIMAL(TO_VARCHAR(id), 18, 0) as id,
+
   name::varchar as name,
-  try_to_number(data_intr_time) as data_intr_time,
-  try_to_timestamp(date) as date_,
+
+  /* data_intr_time NUMERIC(18,0) */
+  TRY_TO_DECIMAL(TO_VARCHAR(data_intr_time), 18, 0) as data_intr_time,
+
+  /* date_ TIMESTAMP NOT NULL UNIQUE */
+  TRY_TO_TIMESTAMP(TO_VARCHAR(date)) as date_,
+
   signal_strength::varchar as signal_strength,
-  try_to_number(power_voltage) as power_voltage,
-  try_to_number(temp) as temp,
-  try_to_number(cumtemp) as cumtemp,
-  try_to_number(humi) as humi,
-  try_to_number(uv) as uv,
-  try_to_number(uvi) as uvi,
-  try_to_number(illumi) as illumi,
-  try_to_number(rain) as rain,
-  try_to_number(rain_1_h) as rain_1h,
-  try_to_number(rain_24_h) as rain_24h,
-  try_to_number(rain_bgn) as rain_bgn,
-  try_to_number(wind_dir) as wind_dir,
+
+  /* power_voltage NUMERIC */
+  TRY_TO_NUMBER(TO_VARCHAR(power_voltage)) as power_voltage,
+
+  /* temp/cumtemp/humi/illumi/rain系/wind_speed系 は NUMERIC（小数あり得る想定でNUMBERに） */
+  TRY_TO_NUMBER(TO_VARCHAR(temp)) as temp,
+  TRY_TO_NUMBER(TO_VARCHAR(cumtemp)) as cumtemp,
+  TRY_TO_NUMBER(TO_VARCHAR(humi)) as humi,
+
+  /* uv/uvi NUMERIC(18,0) */
+  TRY_TO_DECIMAL(TO_VARCHAR(uv), 18, 0) as uv,
+  TRY_TO_DECIMAL(TO_VARCHAR(uvi), 18, 0) as uvi,
+
+  TRY_TO_NUMBER(TO_VARCHAR(illumi)) as illumi,
+  TRY_TO_NUMBER(TO_VARCHAR(rain)) as rain,
+  TRY_TO_NUMBER(TO_VARCHAR(rain_1_h)) as rain_1h,
+  TRY_TO_NUMBER(TO_VARCHAR(rain_24_h)) as rain_24h,
+  TRY_TO_NUMBER(TO_VARCHAR(rain_bgn)) as rain_bgn,
+
+  /* wind_dir NUMERIC(18,0) */
+  TRY_TO_DECIMAL(TO_VARCHAR(wind_dir), 18, 0) as wind_dir,
   wind_dir_str::varchar as wind_dir_str,
-  try_to_number(wind_speed) as wind_speed,
-  try_to_number(max_wind_speed) as max_wind_speed,
-  try_to_number(wbgt) as wbgt,
+
+  TRY_TO_NUMBER(TO_VARCHAR(wind_speed)) as wind_speed,
+  TRY_TO_NUMBER(TO_VARCHAR(max_wind_speed)) as max_wind_speed,
+
+  /* wbgt/atmos NUMERIC(18,0) */
+  TRY_TO_DECIMAL(TO_VARCHAR(wbgt), 18, 0) as wbgt,
   wbgt_str::varchar as wbgt_str,
-  try_to_number(atmos) as atmos,
+  TRY_TO_DECIMAL(TO_VARCHAR(atmos), 18, 0) as atmos,
+
   sea_level_pressure::varchar as sea_level_pressure
+
 from {{ source('raw', 'MIMAWARI') }}
