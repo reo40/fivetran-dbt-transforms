37e778f95c941b4e0b2ac5e9277cbc461d317864
reo40 <r_miyahara@jwts.co.jp>
2026-02-03 17:09:45 +0900

    Update test2.sql

--- models/test2.sql
+++ models/test2.sql
@@ -1,8 +1,8 @@
 {{ config(materialized='view') }}
 
 select
-  /* format_version NUMERIC */
-  TRY_TO_NUMBER(TO_VARCHAR(format_version)) as format_version,
+  /* format_version NUMERIC (Snowflakeだと実質 NUMBER(38,0) なので整数でOK想定) */
+  TRY_TO_DECIMAL(TO_VARCHAR(format_version), 38, 0) as format_version,
 
   /* id NUMERIC(18,0) */
   TRY_TO_DECIMAL(TO_VARCHAR(id), 18, 0) as id,
@@ -17,30 +17,30 @@ select
 
   signal_strength::varchar as signal_strength,
 
-  /* power_voltage NUMERIC */
-  TRY_TO_NUMBER(TO_VARCHAR(power_voltage)) as power_voltage,
+  /* power_voltage NUMERIC（小数の可能性があるなら 2桁、なければ 0桁でもOK） */
+  TRY_TO_DECIMAL(TO_VARCHAR(power_voltage), 18, 2) as power_voltage,
 
-  /* temp/cumtemp/humi/illumi/rain系/wind_speed系 は NUMERIC（小数あり得る想定でNUMBERに） */
-  TRY_TO_NUMBER(TO_VARCHAR(temp)) as temp,
-  TRY_TO_NUMBER(TO_VARCHAR(cumtemp)) as cumtemp,
-  TRY_TO_NUMBER(TO_VARCHAR(humi)) as humi,
+  /* 小数あり得る系：2桁保持（必要なら 3 に変更） */
+  TRY_TO_DECIMAL(TO_VARCHAR(temp), 18, 2) as temp,
+  TRY_TO_DECIMAL(TO_VARCHAR(cumtemp), 18, 2) as cumtemp,
+  TRY_TO_DECIMAL(TO_VARCHAR(humi), 18, 2) as humi,
 
   /* uv/uvi NUMERIC(18,0) */
   TRY_TO_DECIMAL(TO_VARCHAR(uv), 18, 0) as uv,
   TRY_TO_DECIMAL(TO_VARCHAR(uvi), 18, 0) as uvi,
 
-  TRY_TO_NUMBER(TO_VARCHAR(illumi)) as illumi,
-  TRY_TO_NUMBER(TO_VARCHAR(rain)) as rain,
-  TRY_TO_NUMBER(TO_VARCHAR(rain_1_h)) as rain_1h,
-  TRY_TO_NUMBER(TO_VARCHAR(rain_24_h)) as rain_24h,
-  TRY_TO_NUMBER(TO_VARCHAR(rain_bgn)) as rain_bgn,
+  TRY_TO_DECIMAL(TO_VARCHAR(illumi), 18, 2) as illumi,
+  TRY_TO_DECIMAL(TO_VARCHAR(rain), 18, 2) as rain,
+  TRY_TO_DECIMAL(TO_VARCHAR(rain_1_h), 18, 2) as rain_1h,
+  TRY_TO_DECIMAL(TO_VARCHAR(rain_24_h), 18, 2) as rain_24h,
+  TRY_TO_DECIMAL(TO_VARCHAR(rain_bgn), 18, 2) as rain_bgn,
 
   /* wind_dir NUMERIC(18,0) */
   TRY_TO_DECIMAL(TO_VARCHAR(wind_dir), 18, 0) as wind_dir,
   wind_dir_str::varchar as wind_dir_str,
 
-  TRY_TO_NUMBER(TO_VARCHAR(wind_speed)) as wind_speed,
-  TRY_TO_NUMBER(TO_VARCHAR(max_wind_speed)) as max_wind_speed,
+  TRY_TO_DECIMAL(TO_VARCHAR(wind_speed), 18, 2) as wind_speed,
+  TRY_TO_DECIMAL(TO_VARCHAR(max_wind_speed), 18, 2) as max_wind_speed,
 
   /* wbgt/atmos NUMERIC(18,0) */
   TRY_TO_DECIMAL(TO_VARCHAR(wbgt), 18, 0) as wbgt,
