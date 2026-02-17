0bfb150f1cf65143679bfdbc9c443e6975590508
reo40 <r_miyahara@jwts.co.jp>
2026-02-17 14:34:37 +0900

    Delete models/step2_test.sql

--- models/step2_test.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view') }}
-
-SELECT
-  s.CLASS,
-  s.NAME,
-  cb.BUSHO
-FROM {{ ref('step1_test') }} s
-LEFT JOIN {{ source('manual', 'CLASS_BUSHO') }} cb
-  ON s.CLASS = cb.CLASS
