d276728cdeea87d51992c5fd6cae1049e50225c4
reo40 <r_miyahara@jwts.co.jp>
2026-02-17 14:51:17 +0900

    Update t_mimawari.sql

--- models/t_mimawari.sql
+++ models/t_mimawari.sql
@@ -36,47 +36,58 @@ with src as (
 
 mapped as (
   select
-    try_to_number(FORMAT_VERSION, 38, 1)                    as FORMAT_VERSION,
-    ID                                                      as ID,
-    NAME                                                    as NAME,
-    DATA_INTR_TIME                                          as DATA_INTR_TIME,
+    /* number系（混在にも強い形に） */
+    cast(round(try_to_double(FORMAT_VERSION), 1) as number(38,1)) as FORMAT_VERSION,
+
+    ID                                                     as ID,
+    NAME                                                   as NAME,
+    DATA_INTR_TIME                                         as DATA_INTR_TIME,
 
     /* raw: '2026/1/28  23:57:00' (spaces may vary) */
     to_timestamp_ntz(
       regexp_replace(DATE, '\\s+', ' '),
       'YYYY/M/D HH24:MI:SS'
-    )                                                       as DATE_,
+    )                                                      as DATE_,
 
-    SIGNAL_STRENGTH                                         as SIGNAL_STRENGTH,
+    SIGNAL_STRENGTH                                        as SIGNAL_STRENGTH,
 
-    /* raw FLOAT -> target NUMBER(38,1) */
-    try_to_number(POWER_VOLTAGE, 38, 1)                     as POWER_VOLTAGE,
-    try_to_number(TEMP, 38, 1)                              as TEMP,
-    try_to_number(CUMTEMP, 38, 1)                           as CUMTEMP,
+    /* raw FLOAT -> target NUMBER(38,1)
+       TRY_CAST(FLOAT AS NUMBER(38,1)) がSnowflakeで落ちるケースがあるので
+       round + cast に統一（NULLも自然に通る）
+    */
+    cast(round(try_to_double(POWER_VOLTAGE), 1) as number(38,1)) as POWER_VOLTAGE,
+    cast(round(try_to_double(TEMP), 1)          as number(38,1)) as TEMP,
+    cast(round(try_to_double(CUMTEMP), 1)       as number(38,1)) as CUMTEMP,
 
-    try_to_number(HUMI, 38, 1)                              as HUMI,
-    UV                                                      as UV,
-    UVI                                                     as UVI,
-    try_to_number(ILLUMI, 38, 1)                            as ILLUMI,
+    /* rawはNUMBER(38,1)だが、混在しても落ちにくいよう統一 */
+    cast(round(try_to_double(HUMI), 1) as number(38,1))         as HUMI,
 
-    try_to_number(RAIN, 38, 1)                              as RAIN,
-    try_to_number(RAIN_1_H, 38, 1)                          as RAIN_1H,
-    try_to_number(RAIN_24_H, 38, 1)                         as RAIN_24H,
-    try_to_number(RAIN_BGN, 38, 1)                          as RAIN_BGN,
+    UV                                                     as UV,
+    UVI                                                    as UVI,
 
-    WIND_DIR                                                as WIND_DIR,
-    WIND_DIR_STR                                            as WIND_DIR_STR,
-    try_to_number(WIND_SPEED, 38, 1)                        as WIND_SPEED,
-    try_to_number(MAX_WIND_SPEED, 38, 1)                    as MAX_WIND_SPEED,
+    cast(round(try_to_double(ILLUMI), 1) as number(38,1))       as ILLUMI,
+    cast(round(try_to_double(RAIN), 1) as number(38,1))         as RAIN,
 
-    WBGT                                                    as WBGT,
-    WBGT_STR                                                as WBGT_STR,
-    ATMOS                                                   as ATMOS,
-    SEA_LEVEL_PRESSURE                                      as SEA_LEVEL_PRESSURE
+    /* 列名差分：RAIN_1_H -> RAIN_1H / RAIN_24_H -> RAIN_24H */
+    cast(round(try_to_double(RAIN_1_H), 1) as number(38,1))     as RAIN_1H,
+    cast(round(try_to_double(RAIN_24_H), 1) as number(38,1))    as RAIN_24H,
+
+    cast(round(try_to_double(RAIN_BGN), 1) as number(38,1))     as RAIN_BGN,
+
+    WIND_DIR                                               as WIND_DIR,
+    WIND_DIR_STR                                           as WIND_DIR_STR,
+
+    cast(round(try_to_double(WIND_SPEED), 1) as number(38,1))   as WIND_SPEED,
+    cast(round(try_to_double(MAX_WIND_SPEED), 1) as number(38,1)) as MAX_WIND_SPEED,
+
+    WBGT                                                   as WBGT,
+    WBGT_STR                                               as WBGT_STR,
+    ATMOS                                                  as ATMOS,
+    SEA_LEVEL_PRESSURE                                     as SEA_LEVEL_PRESSURE
 
   from src
 )
 
 select *
 from mapped
-where DATE_ is not null   -- targetが NOT NULL のため
+where DATE_ is not null
