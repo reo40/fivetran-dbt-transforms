cd3295219e0410beea756afd16c9483fa9ebee5c
reo40 <r_miyahara@jwts.co.jp>
2026-03-03 15:48:49 +0900

    Update t_mimawari.sql

--- models/t_mimawari.sql
+++ models/t_mimawari.sql
@@ -42,39 +42,28 @@ mapped as (
     TRY_TO_NUMERIC(TO_VARCHAR(ID), 18, 0)              as ID,
     NAME::varchar                                      as NAME,
     TRY_TO_NUMERIC(TO_VARCHAR(DATA_INTR_TIME), 18, 0)  as DATA_INTR_TIME,
-
     TRY_TO_TIMESTAMP_TZ(
       regexp_replace(TO_VARCHAR(DATE), '\\s+', ' '),
       'YYYY/MM/DD HH24:MI:SS'
     )                                                  as DATE_,
-
     SIGNAL_STRENGTH::varchar                           as SIGNAL_STRENGTH,
-
     TRY_TO_NUMERIC(TO_VARCHAR(POWER_VOLTAGE), 38, 3)   as POWER_VOLTAGE,
     TRY_TO_NUMERIC(TO_VARCHAR(TEMP), 38, 3)            as TEMP,
     TRY_TO_NUMERIC(TO_VARCHAR(CUMTEMP), 38, 3)         as CUMTEMP,
     TRY_TO_NUMERIC(TO_VARCHAR(HUMI), 38, 3)            as HUMI,
-
     TRY_TO_NUMERIC(TO_VARCHAR(UV), 18, 0)              as UV,
     TRY_TO_NUMERIC(TO_VARCHAR(UVI), 18, 0)             as UVI,
-
     TRY_TO_NUMERIC(TO_VARCHAR(ILLUMI), 38, 3)          as ILLUMI,
     TRY_TO_NUMERIC(TO_VARCHAR(RAIN), 38, 3)            as RAIN,
-
     TRY_TO_NUMERIC(TO_VARCHAR(RAIN_1_H), 38, 3)        as RAIN_1H,
     TRY_TO_NUMERIC(TO_VARCHAR(RAIN_24_H), 38, 3)       as RAIN_24H,
-
     TRY_TO_NUMERIC(TO_VARCHAR(RAIN_BGN), 38, 3)        as RAIN_BGN,
-
     TRY_TO_NUMERIC(TO_VARCHAR(WIND_DIR), 18, 0)        as WIND_DIR,
     WIND_DIR_STR::varchar                              as WIND_DIR_STR,
-
     TRY_TO_NUMERIC(TO_VARCHAR(WIND_SPEED), 38, 3)      as WIND_SPEED,
     TRY_TO_NUMERIC(TO_VARCHAR(MAX_WIND_SPEED), 38, 3)  as MAX_WIND_SPEED,
-
     TRY_TO_NUMERIC(TO_VARCHAR(WBGT), 18, 0)            as WBGT,
     WBGT_STR::varchar                                  as WBGT_STR,
-
     TRY_TO_NUMERIC(TO_VARCHAR(ATMOS), 18, 0)           as ATMOS,
     SEA_LEVEL_PRESSURE::varchar                        as SEA_LEVEL_PRESSURE
 
